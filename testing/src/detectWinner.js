e80086ad59c42cafb3ee0468a7db2bc5039771e4
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-08 21:38:32 -0400

    UPDATED detectWinner test

--- testing/src/detectWinner.js
+++ testing/src/detectWinner.js
@@ -1,10 +1,4 @@
 export function detectWinner(board){
-    const isRY = (char) => {
-        if(char === 'R' || char === 'Y'){
-            return true;
-        }
-        return false;
-    }
     for(let i = 0; i < 7; i++){
         if(board[i].includes("YYYY") || board[i].includes("RRRR")){
             return true;
@@ -12,10 +6,35 @@ export function detectWinner(board){
     }
     for(let i = 0; i < 4; i++){
         for(let j = 0; j < 6; j++){
-            if(isRY(board[i][j]) && isRY(board[i+1][j]) && isRY(board[i+2][j]) && isRY(board[i+3][j])){
+            if(board[i][j] === 'Y' && board[i+1][j] === 'Y' && board[i+2][j] === 'Y' && board[i+3][j] === 'Y'){
+                return true
+            }
+            if(board[i][j] === 'R' && board[i+1][j] === 'R' && board[i+2][j] === 'R' && board[i+3][j] === 'R'){
                 return true
             }
         }
     }
+    // detect postive slope diagonals
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 3; j++){
+            if(board[i][j] === 'Y' && board[i+1][j+1] === 'Y' && board[i+2][j+2] === 'Y' && board[i+3][j+3] === 'Y'){
+                return true;
+            }
+            if(board[i][j] === 'R' && board[i+1][j+1] === 'R' && board[i+2][j+2] === 'R' && board[i+3][j+3] === 'R'){
+                return true;
+            }
+        }
+    }
+    for(let i = 0; i < 4; i++){
+        for(let j = 3; j < 6; j++){
+            if(board[i][j] === 'Y' && board[i+1][j-1] === 'Y' && board[i+2][j-2] === 'Y' && board[i+3][j-3] === 'Y'){
+                return true;
+            }
+            if(board[i][j] === 'R' && board[i+1][j-1] === 'R' && board[i+2][j-2] === 'R' && board[i+3][j-3] === 'R'){
+                return true;
+            }
+        }
+    }
+    
     return false;
 }
--- testing/tests/detectWinner.spec.js
+++ testing/tests/detectWinner.spec.js
@@ -4,16 +4,28 @@ describe('detectWinner', () => {
         const result = detectWinner(["","","","","","",""])
         expect(result).toEqual(false)
     })
+    it('returns false when passed a board with lots of Ys and Rs but no wins', () => {
+        const result = detectWinner(["RYRYRY", "YRYRYR", "YYYRRR", "R", "Y", "YRYRYR", ""])
+        expect(result).toEqual(false)
+    })
     it('returns true when passed a vertical row of 4', () => {
         const result = detectWinner(["YYYRY","","","","","","RRRR"])
         expect(result).toEqual(true)
     })
     it('returns true when passed horizontal row of 4', () => {
-        const result = detectWinner(["Y","Y","Y","Y","","",""])
+        const result = detectWinner(["R","R","R","R","","",""])
         expect(result).toEqual(true)
     })
     it('returns true when passed horizontal row of 4 in the upper corner', () => {
         const result = detectWinner(["","","","XXXXXY","XXXXXY","XXXXXY","XXXXXY"])
+        expect(result).toEqual(true)
+    })
+    it('returns true when passed diagonal row of 4 with a positive slope', () => {
+        const result = detectWinner(["","","","XXY","XXXY","XXXXY","XXXXXY"])
+        expect(result).toEqual(true)
+    })
+    it('returns true when passed diagonal row of 4 with a negative slope', () => {
+        const result = detectWinner(["","","","XXXXXY","XXXXY","XXXY","XXY"])
         expect(result).toEqual(true)
     })
 })
