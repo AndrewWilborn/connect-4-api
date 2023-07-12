8a2c2914003447d10649c4bb8795bd33d158f26e
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-12 19:48:20 -0400

    ADDED disconnect when players close tab

--- functions/getFunctions.js
+++ functions/getFunctions.js
@@ -6,16 +6,28 @@ const docId = "P06lG9GlRfpHhhMCuTMP"
 export async function joinRequest(req, res){
     const game = await coll.get();
     // There should be a way to get this by ID
-    const { inGame, playerIds } = game.docs[0].data() // Get current data from the database
+    let { inGame, playerIds, lastPing } = game.docs[0].data() // Get current data from the database
     let playerId = null;
     let player0or1 = null;
+    const time = Date.now();
+    const mostRecentPing = lastPing.reduce((a, b) => Math.max(a, b), -Infinity)
+    if(time - mostRecentPing > 10000) {
+        await coll.doc(docId).update({
+            "playerIds": [0,0], 
+            "inGame": false, 
+            "activePlayer": 0, 
+            "board": ["","","","","","",""]
+        });
+        inGame=false;
+        playerIds=[0,0];
+    }
     if(!inGame){ // If there isn't currently a game in progress
         playerId = Math.floor(Math.random() * 999999999) // Generate a playerId
         if(!playerIds[0]){ // update the database with the new playerId
-            await coll.doc(docId).update({"playerIds": [playerId, playerIds[1]]})
+            await coll.doc(docId).update({"playerIds": [playerId, playerIds[1]], lastPing: [time, lastPing[1]]})
             player0or1 = 0;
         } else {
-            await coll.doc(docId).update({"playerIds": [playerIds[0], playerId], "inGame": true})
+            await coll.doc(docId).update({"playerIds": [playerIds[0], playerId], lastPing: [lastPing[0], time], "inGame": true})
             player0or1 = 1;
         }
     } else {
@@ -41,9 +53,17 @@ export async function getGameState(req, res){
     const {inGame, board, activePlayer, lastPing} = game.docs[0].data();
     const time = Date.now();
     if(whichPlayer == 0){ // Use loose equality because we expect whichPlayer to be a string
-        await coll.doc(docId).update({"lastPing": [time, lastPing[1]]})
+        if(time - 5000 > lastPing[1] && inGame){
+            await coll.doc(docId).update({"lastPing": [time, lastPing[1]], "activePlayer": 4})
+        } else{
+            await coll.doc(docId).update({"lastPing": [time, lastPing[1]]})
+        }
     } else {
-        await coll.doc(docId).update({"lastPing": [lastPing[0], time]})
+        if(time - 5000 > lastPing[0] && inGame){
+            await coll.doc(docId).update({"lastPing": [lastPing[0], time], "activePlayer": 4})
+        } else{
+            await coll.doc(docId).update({"lastPing": [lastPing[0], time]})
+        }
     }
     res.status(200).send({"inGame": inGame, "activePlayer": activePlayer, "board": board});
 }
